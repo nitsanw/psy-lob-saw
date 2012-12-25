c860117caa860674c42a3296048d85b0b3564fac
nitsan <nitsan@PushCart>
2012-12-25 13:14:25 +0000

    replace while with for, slight tidy.

--- experiments/alignment/UnsafeCacheAlignmentCostBenchmark.java
+++ experiments/alignment/UnsafeCacheAlignmentCostBenchmark.java
@@ -3,73 +3,39 @@ package alignment;
 import java.nio.ByteBuffer;
 
 import util.UnsafeAccess;
-import util.UnsafeDirectByteBuffer;
+import static util.UnsafeDirectByteBuffer.*;
 
+import com.google.caliper.Param;
 import com.google.caliper.SimpleBenchmark;
 
 public class UnsafeCacheAlignmentCostBenchmark extends SimpleBenchmark {
+    @Param(value = "1")
+    int offset;
 
-	private static final int CAPACITY = 16 * 4096;
-	// buffy is a page aligned 4 page sized buffer, and a vampire slayer
-	private ByteBuffer buffy = UnsafeDirectByteBuffer
-			.allocateAlignedByteBuffer(CAPACITY, 4096);
+    private static final int CAPACITY = PAGE_SIZE;
+    // buffy is a page aligned buffer, and a vampire slayer
+    private ByteBuffer buffy = allocateAlignedByteBuffer(CAPACITY, PAGE_SIZE);
 
-	public int timeBrokenLong(final int reps) throws InterruptedException {
-		long remaining=0;
-		for (long i = 0; i < reps; i++) {
-			final long startingAddress = UnsafeDirectByteBuffer.getAddress(buffy) + 60;
-			final long limit = UnsafeDirectByteBuffer.getAddress(buffy) + CAPACITY;
-			remaining=writeAndRead(i, startingAddress, limit);
-		}
-		return (int)remaining;
+    public int timeOffsetLongAccess(final int reps) throws InterruptedException {
+	long remaining = 0;
+	for (long i = 0; i < reps; i++) {
+	    final long startingAddress = getAddress(buffy) + offset;
+	    final long limit = getAddress(buffy) + CAPACITY;
+	    remaining = writeAndRead(i, startingAddress, limit);
 	}
+	return (int) remaining;
+    }
 
-	private long  writeAndRead(final long value, final long startingAddress,
-			final long limit) {
-		long address = startingAddress;
-		do {
-			UnsafeAccess.unsafe.putLong(address, value);
-			address += UnsafeDirectByteBuffer.CACHE_LINE_SIZE;
-		} while (address < limit);
-		address = startingAddress;
-		do {
-			if (UnsafeAccess.unsafe.getLong(address) != value)
-				throw new RuntimeException();
-			address += UnsafeDirectByteBuffer.CACHE_LINE_SIZE;
-		} while (address < limit);
-		return  limit- address + UnsafeDirectByteBuffer.CACHE_LINE_SIZE;
+    private long writeAndRead(final long value, final long startingAddress,
+	    final long limit) {
+	long address;
+	for (address = startingAddress; address < limit; address += CACHE_LINE_SIZE) {
+	    UnsafeAccess.unsafe.putLong(address, value);
 	}
-
-	public int timeFirstLong(final int reps) throws InterruptedException {
-
-		long remaining=0;
-		for (long i = 0; i < reps; i++) {
-			final long startingAddress = UnsafeDirectByteBuffer.getAddress(buffy);
-			final long limit = UnsafeDirectByteBuffer.getAddress(buffy) + CAPACITY;
-			remaining=writeAndRead(i, startingAddress, limit);
-		}
-		return (int)remaining;
+	for (address = startingAddress; address < limit; address += CACHE_LINE_SIZE) {
+	    if (UnsafeAccess.unsafe.getLong(address) != value)
+		throw new RuntimeException();
 	}
-
-	public int timeLastLong(final int reps) throws InterruptedException {
-
-		long remaining=0;
-		for (long i = 0; i < reps; i++) {
-			final long startingAddress = UnsafeDirectByteBuffer.getAddress(buffy) + 56;
-			final long limit = UnsafeDirectByteBuffer.getAddress(buffy) + CAPACITY;
-			remaining=writeAndRead(i, startingAddress, limit);
-		}
-		return (int)remaining;
-	}
-	
-	public int timeMisalignedLong(final int reps) throws InterruptedException {
-
-		long remaining=0;
-		for (long i = 0; i < reps; i++) {
-			final long startingAddress = UnsafeDirectByteBuffer.getAddress(buffy) + 1;
-			final long limit = UnsafeDirectByteBuffer.getAddress(buffy) + CAPACITY;
-			remaining=writeAndRead(i, startingAddress, limit);
-		}
-		return (int)remaining;
-	}
+	return limit - address + CACHE_LINE_SIZE;
+    }
 }
